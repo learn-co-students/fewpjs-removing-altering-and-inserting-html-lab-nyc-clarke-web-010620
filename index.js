8c34acf0e75bbf203f87ce2772fff6e507a8a64d
github username <joemokhtarali@gmail.com>
2020-02-23 20:20:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+
+    let ele = document.querySelector('main#main').remove()
+
+    let newHeader = document.createElement('h1')
+     newHeader.id = 'victory'
+    
+
+    newHeader.innerHTML = "Flatiron School is the champion!"
+    newHeader.className = 'victory'
+
+    
